11eab18da30673182899675ceb888fc2fbadc547
Nora Heaphy <nora.heaphy@yale.edu>
2020-11-30 12:01:30 -0500

    adding suppl climate niche scripts

--- scripts/australia_null_climate.R
+++ scripts/australia_null_climate.R
@@ -28,8 +28,8 @@ australia_null$CHELSA_12 <- as.numeric(australia_null$CHELSA_12)
 
 # transform CHELSA_BIOL_01 to MAT, rename things, lat and lon got switched
 australia_null <- australia_null %>% mutate(MAT = CHELSA_01*0.1) %>%
-  rename("MAP" = "CHELSA_12", "lon" = "decimalLatitude", "lat" = "decimalLongitude") +
-  select(-CHELSA_01)
+  rename("MAP" = "CHELSA_12", "lon" = "decimalLatitude", "lat" = "decimalLongitude") 
+australia_null <- australia_null %>% select(-CHELSA_01)
 
 # add a bogus species column to make the seas test work
 australia_null <- australia_null %>% mutate(species = "species")
@@ -56,4 +56,38 @@ australia_null_no_outliers <- australia_null %>% filter(MAP < 3000 & MAT > 5.1)
 ggplot(australia_null_no_outliers, aes(x = MAT, y = MAP)) +
   geom_point(size = 0.5) + 
   labs(title = "Australia Null Climate", x = "Mean Annual Temperature (degrees C)", 
+       y = "Mean Annual Precipitation (mm/year)")
+
+## bring in Portulaca and Calandrinia data to add onto biplot
+
+calandrinia <- read.csv("cal_port_aus_10-24-2020.csv", header = TRUE, sep = ",",
+                     na.strings = "") %>%
+  filter(clade == "Calandrinia")
+
+portulaca <- read.csv("cal_port_aus_10-24-2020.csv", header = TRUE, sep = ",",
+                     na.strings = "") %>%
+  filter(clade == "Portulaca")
+
+# transform CHELSA_BIOL_01 to MAT
+portulaca <- portulaca %>% mutate(MAT = CHELSA_BIOL_01*0.1) %>%
+  rename("MAP" = "CHELSA_BIOL_12")
+anacamps <- anacamps %>% mutate(MAT = CHELSA_BIOL_01*0.1) %>%
+  rename("MAP" = "CHELSA_BIOL_12")
+calandrinia <- calandrinia %>% mutate(MAT = CHELSA_BIOL_01*0.1) %>%
+  rename("MAP" = "CHELSA_BIOL_12")
+
+# plot null and Portulaca together
+ggplot(australia_null_no_outliers, aes(x = MAT, y = MAP)) +
+  geom_point(size = 1, color = "grey") + 
+  geom_point(data = portulaca, color = "black", size = 0.3) +
+  labs(title = "Australia Null Climate (with Portulaca)", 
+       x = "Mean Annual Temperature (degrees C)", 
+       y = "Mean Annual Precipitation (mm/year)")
+  
+# plot null and Calandrinia together
+ggplot(australia_null_no_outliers, aes(x = MAT, y = MAP)) +
+  geom_point(size = 1, color = "grey") + 
+  geom_point(data = calandrinia, color = "black", size = 0.3) +
+  labs(title = "Australia Null Climate (with Calandrinia)", 
+       x = "Mean Annual Temperature (degrees C)", 
        y = "Mean Annual Precipitation (mm/year)")
--- scripts/portulaca_clipped_to_anacamps.R
+++ scripts/portulaca_clipped_to_anacamps.R
@@ -81,115 +81,115 @@ ggplot(port_ana_clipped, aes(x = MAT, y = CHELSA_BIOL_12, color = clade)) +
 ########### KERNEL DENSITY PLOTS ###########
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_01, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Annual Temperature") +
   xlab("Mean Annual Temperature [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_02, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Diurnal Range") +
   xlab("Mean Diurnal Range [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_03, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Isothermality") +
   xlab("Isothermality")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_04, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Temperature Seasonality") +
   xlab("Temperature Seasonality [standard deviation]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_05, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Max Temperature of Warmest Month") +
   xlab("Max Temperature of Warmest Month [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_06, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Min Temperature of Coldest Month") +
   xlab("Min Temperature of Coldest Month [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_07, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Temperature Annual Range") +
   xlab("Temperature Annual Range [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_08, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Temperature of Wettest Quarter") +
   xlab("Mean Temperature of Wettest Quarter [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_09, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Temperature of Driest Quarter") +
   xlab("Mean Temperature of Driest Quarter [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_10, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Temperature of Warmest Quarter") +
   xlab("Mean Temperature of Warmest Quarter [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_11, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Temperature of Coldest Quarter") +
   xlab("Mean Temperature of Coldest Quarter [°C*10]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_12, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Mean Annual Precipitation") +
   xlab("Mean Annual Precipitation [mm/year]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_13, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Wettest Month") +
   xlab("Precipitation of Wettest Month [mm/month]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_14, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Driest Month") +
   xlab("Precipitation of Driest Month [mm/month]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_15, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation Seasonality") +
   xlab("Precipitation Seasonality [coefficient of variation]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_16, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Wettest Quarter") +
   xlab("Precipitation of Wettest Quarter [mm/quarter]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_17, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Driest Quarter") +
   xlab("Precipitation of Driest Quarter [mm/quarter]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_18, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Warmest Quarter") +
   xlab("Precipitation of Warmest Quarter [mm/quarter]")
 
 ggplot(port_ana_clipped, aes(x = CHELSA_BIOL_19, y = ..scaled.., fill = clade)) + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   geom_density(alpha = 0.5) + 
   ggtitle("Precipitation of Coldest Quarter") +
   xlab("Precipitation of Coldest Quarter [mm/quarter]")
@@ -201,133 +201,133 @@ my_comparisons <- list(c("Portulaca", "Anacampserotaceae"))
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_01, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Annual Temperature", x = "clade", 
        y = "Mean Annual Temperature [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_02, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Diurnal Range", x = "clade", 
        y = "Mean Diurnal Range [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_03, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Isothermality", x = "clade", 
        y = "Isothermality") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_04, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Temperature Seasonality", x = "clade", 
        y = "Temperature Seasonality [standard deviation]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_05, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Max Temperature of Warmest Month", x = "clade", 
        y = "Max Temperature of Warmest Month [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_06, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Min Temperature of Coldest Month", x = "clade", 
        y = "Min Temperature of Coldest Month [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_07, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Temperature Annual Range", x = "clade", 
        y = "Temperature Annual Range [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_08, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Temperature of Wettest Quarter", x = "clade", 
        y = "Mean Temperature of Wettest Quarter [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_09, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Temperature of Driest Quarter", x = "clade", 
        y = "Mean Temperature of Driest Quarter [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_10, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Temperature of Warmest Quarter", x = "clade", 
        y = "Mean Temperature of Warmest Quarter [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_11, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Temperature of Coldest Quarter", x = "clade", 
        y = "Mean Temperature of Coldest Quarter [°C*10]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_12, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Mean Annual Precipitation", x = "clade", 
        y = "Mean Annual Precipitation [mm/year]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_13, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Wettest Month", x = "clade", 
        y = "Precipitation of Wettest Month [mm/month]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_14, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Driest Month", x = "clade", 
        y = "Precipitation of Driest Month [mm/month]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_15, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation Seasonality", x = "clade", 
        y = "Precipitation Seasonality [coefficient of variation]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_16, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Wettest Quarter", x = "clade", 
        y = "Precipitation of Wettest Quarter [mm/quarter]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_17, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Driest Quarter", x = "clade", 
        y = "Precipitation of Driest Quarter [mm/quarter]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_18, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Warmest Quarter", x = "clade", 
        y = "Precipitation of Warmest Quarter [mm/quarter]") +
   stat_compare_means(comparisons = my_comparisons)
 
 ggplot(port_ana_clipped, aes(x = clade, y = CHELSA_BIOL_19, fill = clade)) +
   geom_boxplot() + 
-  scale_fill_manual(values = c("#F8766D", "#00BFC4", "#7CAE00")) +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D")) +
   labs(title = "Precipitation of Coldest Quarter", x = "clade", 
        y = "Precipitation of Coldest Quarter [mm/quarter]") +
   stat_compare_means(comparisons = my_comparisons)
